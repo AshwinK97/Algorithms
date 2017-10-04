a61cf119fab81a389511c2d416383cb6b0ac87c9
AshwinK97 <ashwin.kamalakannan@uoit.net>
2017-10-04 10:13:40 -0400

    pareto function done

--- Assignment 2/solution.py
+++ Assignment 2/solution.py
@@ -5,21 +5,23 @@ import math
 import time
 import sys
 
+# if all u[i] <= v[i] and atleast 1 u[i] < v[i]
 def paretoDominant(u, v):
-    
-    strict = False
+    strict = 0
     for i, j in zip(u, v):
         if i > j:
             return False
         if i < j:
-            strict = True
-    return True, Scrict
+            strict = 1
+    return strict == 1
 
 def exportPlot(u, v, ofile):
     plt.plot(u, color='r', lw='2')
     plt.plot(v, color='b', lw='2')
     plt.savefig(ofile)
 
+######################## MAIN ###########################
 u = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-v = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+v = [1, 2, 3, 4, 5, 6, 7, 9, 9, 10]
 exportPlot(u, v, 'output.png')
+print paretoDominant(u, v)
