61435437eacea5bfbcbe72e89ff723adeaeff3a7
AshwinK97 <ashwin.kamalakannan@uoit.net>
2017-11-30 02:33:08 -0500

    pso and plot system done

--- Final-Project/de.py
+++ Final-Project/de.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 # Function implementations
 hce = lambda x: sum([np.power(1000000.0, (i)/(d-1)) * (xC ** 2) for i, xC in enumerate(x)])
@@ -11,9 +12,6 @@ discus = lambda x: (10**6)*(x[0]**2) + sum(np.power(x[1:], 2))
 
 rosenbrock = lambda x: sum(100 * np.power((np.power(x[:-1], 2) \
 	- x[1:]), 2) + np.power((x[:-1] - 1), 2))
-
-def ackley(x):
-	return -20 * np.exp(-0.2 * np.sqrt(sum(np.power(x, 2))/len(x))) - np.exp(sum(np.cos(2*np.pi*x))/len(x)) + 20 + np.exp(1)
 
 ackley = lambda x: -20 * np.exp(-0.2 * np.sqrt(sum(np.power(x, 2))/len(x))) - \
 	np.exp(sum(np.cos(2*np.pi*x))/len(x)) + 20 + np.exp(1)
@@ -34,58 +32,134 @@ katsuura = lambda x: (10/len(x)) * np.prod(np.power(([1+ i * \
 	range(1, 33))) for i, item in enumerate(x)]), (10 \
 	/ np.power(len(x), 1.2)))) - (10 / np.power(len(x), 2))
 
-def plot3d(fun):
-	fig = plt.figure()
+# plot the specified function in 3 dimensions
+def plotFunction(fun):
+	fig = plt.figure(figsize=(10, 8))
 	ax = fig.add_subplot(111, projection='3d')
 	x = np.linspace(-10, 10, 100)
 	y = np.linspace(-10, 10, 100)
 	X, Y = np.meshgrid(x, y)
 	Z = fun(np.asarray([X, Y]))
-
-	ax.plot_surface(X, Y, Z)
-
+	ax.plot_surface(X, Y, Z, cmap=cm.BuPu)
 	ax.set_xlabel('X axis')
 	ax.set_ylabel('Y axis')
 	ax.set_zlabel('Z axis')
-
 	plt.show()
 
-def de(fobj, bounds, mut=0.8, crossp=0.9, population=100, its=3000):
+# differential evolution algorithm
+def de(fun, bounds, mut=0.8, crossp=0.9, population=100, its=3000):
 	dimensions = len(bounds)
-	pop = np.random.rand(population, dimensions)
+	population = np.random.rand(population, dimensions)
 	min_b, max_b = np.asarray(bounds).T
 	diff = np.fabs(min_b - max_b)
-	pop_denorm = min_b + pop * diff
-	fitness = np.asarray([fobj(ind) for ind in pop_denorm])
+	population_denorm = min_b + population * diff
+	fitness = np.asarray([fun(ind) for ind in population_denorm])
 	best_idx = np.argmin(fitness)
-	best = pop_denorm[best_idx]
+	best = population_denorm[best_idx]
 	for i in range(its):
 		for j in range(population):
 			idxs = [idx for idx in range(population) if idx != j]
-			a, b, c = pop[np.random.choice(idxs, 3, replace = False)]
+			a, b, c = population[np.random.choice(idxs, 3, replace = False)]
 			mutant = np.clip(a + mut * (b - c), 0, 1)
 			cross_points = np.random.rand(dimensions) < crossp
 			if not np.any(cross_points):
 				cross_points[np.random.randint(0, dimensions)] = True
-			trial = np.where(cross_points, mutant, pop[j])
+			trial = np.where(cross_points, mutant, population[j])
 			trial_denorm = min_b + trial * diff
-			f = fobj(trial_denorm)
+			f = fun(trial_denorm)
 			if f < fitness[j]:
 				fitness[j] = f
-				pop[j] = trial
+				population[j] = trial
 				if f < fitness[best_idx]:
 					best_idx = j
 					best = trial_denorm
 		yield best, fitness[best_idx]
 
+# particle swarm algorithm
+def pso(fun, dimensions, min = -10, max = 10, its = 3000, populationSize = 100, c1 = 2.05, c2 = 2.05, w = 0.65, wdamp = 0.995):
+
+    # Empty Particle Template
+    empty_particle = {
+        'position': None,
+        'velocity': None,
+        'cost': None,
+        'best_position': None,
+        'best_cost': None,
+    };
+
+    # Extract Problem Info
+    CostFunction = fun;
+    min = -10;
+    max = 10;
+
+    # Initialize Global Best
+    gbest = {'position': None, 'cost': np.inf};
+
+    # Create Initial population
+    population = [];
+    for i in range(0, populationSize):
+        population.append(empty_particle.copy());
+        population[i]['position'] = np.random.uniform(min, max, dimensions);
+        population[i]['velocity'] = np.zeros(dimensions);
+        population[i]['cost'] = CostFunction(population[i]['position']);
+        population[i]['best_position'] = population[i]['position'].copy();
+        population[i]['best_cost'] = population[i]['cost'];
+        
+        if population[i]['best_cost'] < gbest['cost']:
+            gbest['position'] = population[i]['best_position'].copy();
+            gbest['cost'] = population[i]['best_cost'];
+    
+    # PSO Loop
+    for it in range(0, its):
+        for i in range(0, populationSize):
+            
+            population[i]['velocity'] = w*population[i]['velocity'] \
+                + c1*np.random.rand(dimensions)*(population[i]['best_position'] - population[i]['position']) \
+                + c2*np.random.rand(dimensions)*(gbest['position'] - population[i]['position']);
+
+            population[i]['position'] += population[i]['velocity'];
+            population[i]['position'] = np.maximum(population[i]['position'], min);
+            population[i]['position'] = np.minimum(population[i]['position'], max);
+
+            population[i]['cost'] = CostFunction(population[i]['position']);
+            
+            if population[i]['cost'] < population[i]['best_cost']:
+                population[i]['best_position'] = population[i]['position'].copy();
+                population[i]['best_cost'] = population[i]['cost'];
+
+                if population[i]['best_cost'] < gbest['cost']:
+                    gbest['position'] = population[i]['best_position'].copy();
+                    gbest['cost'] = population[i]['best_cost'];
+
+        w *= wdamp;
+        # print('Iteration {}: Best Cost = {}'.format(it, gbest['cost']));
+
+    	yield gbest['cost'];
+
+
+plotFunction(weierstrass)
 
 # for d in [2, 5, 10]:
 # 	output = list(de(ackley, bounds=[(-10, 10)] * d))
-# 	print(output[-1])
+# 	print(output[-1][1])
 # 	x, f = zip(*output)
 # 	plt.plot(f, label='d={}'.format(d))
 # 	plt.legend()
 # 	plt.show()
 # 	plt.savefig('ackley.png')
 
-plot3d(ackley)
+output = []
+# for d in range (0, 51):
+
+output = list(pso(weierstrass, its = 200, populationSize = 100, c1 = 2.05, c2 = 2.05, w = 0.65, wdamp = 0.995, dimensions=2))
+
+# print list(pso(ackley, its = 200, populationSize = 100, c1 = 2.05, c2 = 2.05, w = 0.65, wdamp = 0.995, dimensions=2))
+
+plt.plot(output)
+plt.show()
+
+# plt.plot(output, label='d={}'.format(d))
+# plt.legend()
+# plt.show()
+# plt.savefig('ackley2')
+
